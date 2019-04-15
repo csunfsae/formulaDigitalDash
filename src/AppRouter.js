912917e41a769e246f3e3c013a72628949fb6c1c
Alejandro Martinez <hello@alejandrom.xyz>
2019-04-15 11:27:26 -0700

    Remote Functions

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Dash from './Dash' ;
+import Home from './Home';
 
 function AppRouter() {
     return (
-        <Router>
-            <Route path="/dash" exact component={Dash} />
-        </Router>
+      <Router>
+        <Route path="/" exact component={Home} />
+        <Route path="/dash" component={Dash} />
+      </Router>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import "./bootstrap.scss";
-import './index.css';
 
 import AppRouter from "./AppRouter";
 
